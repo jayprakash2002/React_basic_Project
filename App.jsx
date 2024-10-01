091285529d3b377de75090b3254d3255d6855f3e
jayprakash2002 <143065634+jayprakash2002@users.noreply.github.com>
2024-10-01 17:38:49 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -4,12 +4,12 @@ import './App.css'
 //import Product from "./Product.jsx";
 import ProductTab from './ProductTab.jsx';
 //import MsgBox from './MsgBox.jsx';
+import Button from "./Button";
 
 function App() {
   return(
     <div>
-      <h2>Blockbuster Deals | Shop Now</h2>
-      <ProductTab />
+      <Button />
     </div>
   );
 }
